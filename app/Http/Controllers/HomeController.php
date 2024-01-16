64c16fcb52492718f8492c6d967108f95b37591d
Kaizhennn <93144035+Kaizhennn@users.noreply.github.com>
2024-01-16 20:52:12 +0800

    31

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
-use App\Models\Membership;
-use App\Notifications\DaftarAhliNotification;
 
 class HomeController extends Controller
 {
