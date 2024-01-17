8a4ffd934dc0dc8a96492f2308a6423572e2a68e
afiqibnuazam <afiqibnuazam@gmail.com>
2024-01-17 16:16:10 +0800

    try fix 1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -73,6 +73,40 @@
             <div class="m-3 w-full relative flex justify-center">
                 <img src="{{ asset('img/PosterKhairat.jpg') }}" alt="Masjid Image" class="w-2/3 h-auto mb-4 rounded">
             </div>
+            
+            @endif
+        </div>
+        
+    </div>
+
+    <div class="bg-white py-20 px-40">
+        <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
+            {{ __('E-Khairat') }}
+        </h2>
+        <div class="p-4 relative flex items-center">
+            <div class="m-3 w-full">
+                <h1 class="font-bold text-3xl">PENDAFTARAN KHAIRAT<br>KEMATIAN TELAH DIBUKA</h1>
+
+                <p class="p-5">
+                    Tabung Khairat Kematian Masjid Taman Sri Pulai, Skudai, Johor (TKKMTSP) 
+                    adalah satu tabung kebajikan yang ditubuhkan oleh Ahli Jawatan Kuasa dan Pegawai Masjid,
+                    Masjid Taman Sri Pulai (MTSP) (sebelum ini Masjid Intan Abu Bakar, Taman Sri Pulai, MIAB) pada 
+                    tahun 2000. TKKMTSP diamanahkan kepada Biro Kebajikan, Sosial dan Kebudayaan, MTSP untuk diuruskan dengan 
+                    lebih teratur dan berkesan mulai tahun 2006.
+                </p>
+                <div class="flex justify-end px-5">
+                    <a href="{{ route('membership.latarBelakang') }}">
+                        <button class="bg-blue-800 text-blue-200 hover:bg-blue-200 hover:text-blue-800 px-9 py-2 rounded-full" type="submit">
+                            {{ __('Info Lanjut') }}
+                        </button>
+                    </a>
+                </div>
+                
+                
+            </div>
+            <div class="m-3 w-full relative flex justify-center">
+                <img src="{{ asset('img/PosterKhairat.jpg') }}" alt="Masjid Image" class="w-2/3 h-auto mb-4 rounded">
+            </div>
         </div>
         
     </div>
