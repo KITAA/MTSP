df033e0be2fadfc33633d0f1e7219e9e9ee5c636
afiqibnuazam <afiqibnuazam@gmail.com>
2024-01-17 15:58:01 +0800

    Merge branch 'Afiq-Azam' of https://github.com/KITAA/MTSP into Afiq-Azam

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,37 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Infaq;
+use App\Models\Berita;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Membership;
+use App\Models\Payment;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $berita = Berita::orderBy('created_at', 'ASC')->get();
+
         if (Auth::check()) {
             $usertype = Auth::user()->usertype;
 
             if ($usertype == 'admin') {
-                return view('admin.dashboard');
+                $totalUser=User::all()->count();
+                $totalMembership=Membership::all()->count();
+                $totalMoney=Payment::all()->sum('price');
+                $totalInfaq=Infaq::all()->sum('donationAmount');
+                $membership=Membership::where('status', 'Dalam proses')->get();
+                $infaq = Infaq::all();
+                
+                return view('admin.dashboard', compact('totalUser', 'totalMembership', 'totalMoney', 'totalInfaq', 'membership', 'infaq'));
             } 
             
             elseif ($usertype == 'user') {
-                return view('dashboard');
+                return view('dashboard', compact('berita'));
             } 
             
             else {
@@ -25,7 +40,7 @@ class HomeController extends Controller
             }
         } else {
             // The user is a guest
-            return view('dashboard');
+            return view('dashboard', compact('berita'));
         }
     }    
 }
